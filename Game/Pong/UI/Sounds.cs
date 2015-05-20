eb98c9772e745187b5139c04e2c7e89266c11cde
AsenT <a.h.temelkov@gmail.com>
2015-05-20 22:00:19 +0300

    Player 2 added, visual and sound effects added. Player Controls ("Q","A","P","L")

---
+++ Game/Pong/UI/Sounds.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong.UI
+{
+    class Sounds
+    {
+        public static void PlayingSounds(string soundeffect)
+        {
+            switch (soundeffect)
+            {
+                case "background":
+                    using (SoundPlayer background = new SoundPlayer(@"Sounds\background.wav"))
+                    {
+                        background.PlayLooping();
+                    }
+                    break;
+                case "gameover":
+                    using (SoundPlayer gameover = new SoundPlayer(@"Sounds\gameover.wav"))
+                    {
+                        gameover.PlaySync();
+                    }
+                    break;
+
+                //TODO
+                /*case "menu":
+                    using (SoundPlayer menu = new SoundPlayer(@"Sounds\....wav"))
+                    {
+                        menu.Play();
+                    }
+                    break;
+                 */
+            }
+        }
+    }
+}
